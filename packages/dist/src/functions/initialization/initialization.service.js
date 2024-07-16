e535a82aca24d95982bca146655ba0d46205e4e3
Lei Xu <komushi@gmail.com>
2024-07-16 13:33:55 +0900

    init

--- packages/dist/src/functions/initialization/initialization.service.js
+++ packages/dist/src/functions/initialization/initialization.service.js
@@ -28,21 +28,31 @@ class InitializationService {
     intializeEnvVar() {
         return __awaiter(this, void 0, void 0, function* () {
             console.log('initialization.service intializeEnvVar in');
-            if (!process.env.HOST_ID || !process.env.STAGE || !process.env.IDENTTITY_ID || !process.env.CRED_PROVIDER_HOST) {
-                const result = yield this.assetsService.getHost();
-                process.env.HOST_ID = result.hostId;
-                process.env.STAGE = result.stage;
-                process.env.IDENTTITY_ID = result.identityId;
-                process.env.CRED_PROVIDER_HOST = result.credProviderHost;
-            }
-            if (!process.env.PROPERTY_CODE) {
-                if (process.env.HOST_ID) {
-                    const property = yield this.assetsService.getProperty(process.env.HOST_ID);
-                    if (property) {
-                        process.env.PROPERTY_CODE = property.propertyCode;
-                        console.log('initialization.service intializeEnvVar Property UUID: ' + property.uuid);
+            try {
+                if (!process.env.HOST_ID || !process.env.STAGE || !process.env.IDENTTITY_ID || !process.env.CRED_PROVIDER_HOST) {
+                    const result = yield this.assetsService.getHost();
+                    process.env.HOST_ID = result.hostId;
+                    process.env.STAGE = result.stage;
+                    process.env.IDENTTITY_ID = result.identityId;
+                    process.env.CRED_PROVIDER_HOST = result.credProviderHost;
+                }
+                if (!process.env.PROPERTY_CODE) {
+                    if (process.env.HOST_ID) {
+                        const property = yield this.assetsService.getProperty(process.env.HOST_ID);
+                        if (property) {
+                            process.env.PROPERTY_CODE = property.propertyCode;
+                            console.log('initialization.service intializeEnvVar Property UUID: ' + property.uuid);
+                        }
                     }
                 }
+            }
+            catch (err) {
+                console.error('!!!!!!error happened at intializeEnvVar!!!!!!');
+                console.error('err.name:' + err.name);
+                console.error('err.message:' + err.message);
+                console.error('err.stack:' + err.stack);
+                console.trace();
+                console.error('!!!!!!error happened at intializeEnvVar!!!!!!');
             }
             console.log('after intializeEnvVar HOST_ID:' + process.env.HOST_ID);
             console.log('after intializeEnvVar IDENTTITY_ID:' + process.env.IDENTTITY_ID);
--- packages/src/functions/initialization/initialization.service.ts
+++ packages/src/functions/initialization/initialization.service.ts
@@ -25,23 +25,32 @@ export class InitializationService {
 
     	console.log('initialization.service intializeEnvVar in');
 
-	    if (!process.env.HOST_ID || !process.env.STAGE || !process.env.IDENTTITY_ID || !process.env.CRED_PROVIDER_HOST) {
-	        const result = await this.assetsService.getHost();
-	        process.env.HOST_ID = result.hostId;
-	        process.env.STAGE = result.stage;
-			process.env.IDENTTITY_ID = result.identityId;
-			process.env.CRED_PROVIDER_HOST = result.credProviderHost;
-	    }
-
-	    if (!process.env.PROPERTY_CODE) {
-	        if (process.env.HOST_ID) {
-	            const property = await this.assetsService.getProperty(process.env.HOST_ID);
-	            if (property) {
-	            	process.env.PROPERTY_CODE = property.propertyCode;
-	            	console.log('initialization.service intializeEnvVar Property UUID: ' + property.uuid);
-	            }
-	        }
-	    }
+		try {
+			if (!process.env.HOST_ID || !process.env.STAGE || !process.env.IDENTTITY_ID || !process.env.CRED_PROVIDER_HOST) {
+				const result = await this.assetsService.getHost();
+				process.env.HOST_ID = result.hostId;
+				process.env.STAGE = result.stage;
+				process.env.IDENTTITY_ID = result.identityId;
+				process.env.CRED_PROVIDER_HOST = result.credProviderHost;
+			}
+	
+			if (!process.env.PROPERTY_CODE) {
+				if (process.env.HOST_ID) {
+					const property = await this.assetsService.getProperty(process.env.HOST_ID);
+					if (property) {
+						process.env.PROPERTY_CODE = property.propertyCode;
+						console.log('initialization.service intializeEnvVar Property UUID: ' + property.uuid);
+					}
+				}
+			}
+		} catch (err) {
+			console.error('!!!!!!error happened at intializeEnvVar!!!!!!');
+			console.error('err.name:' + err.name);
+			console.error('err.message:' + err.message);
+			console.error('err.stack:' + err.stack);
+			console.trace();
+			console.error('!!!!!!error happened at intializeEnvVar!!!!!!');
+		} 
 
 		console.log('after intializeEnvVar HOST_ID:' + process.env.HOST_ID);
 		console.log('after intializeEnvVar IDENTTITY_ID:' + process.env.IDENTTITY_ID);
